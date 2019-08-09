ebb5683aedbd386de7cb8c6fd7e885c44215887a
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-09 06:08:03 -0500

    Make server fetch files from another directory using ejs

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const express = require('express')
+const app = express()
+
+// Make this app find html files
+app.set('views', 'views-html') // First arg must be views
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('home-guest')
+})
+
+app.listen(3000)
