681bcb67dd0ec34ab0e0491bd1c3e89bfd7007df
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-09 06:18:13 -0500

    nodemon, main css

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+// Make this app find files for the public eyes - e.g css
+app.use(express.static('public'))
 // Make this app find html files
 app.set('views', 'views-html') // First arg must be views
 app.set('view engine', 'ejs')
