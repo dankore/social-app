f84f87436df2262a36aad4c5413d06821e421135
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-09 21:46:20 -0500

    User: Use validator to validate email

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const validaor = require("validator")
+const validator = require("validator")
 
 //This is a constructor function
 // Throws error if in arrow function
@@ -9,15 +9,15 @@ let User = function (data) {
 
 User.prototype.validate = function () {
     if (this.data.username == "") { this.errors.push("You must provide a username.") }
-    if (this.data.email == "") { this.errors.push("You must provide a valid email address.") }
+    if (!validator.isEmail(this.data.email)) { this.errors.push("You must provide a valid email address.") }
     if (this.data.password == "") { this.errors.push("You must provide a password.") }
 
     //Password be at least 8 characters
-    if (this.data.length.password.length > 0 && this.data.password.length < 12) { this.errors.push("Password must be at least 12 characters.") }
+    if (this.data.password.length > 0 && this.data.password.length < 12) { this.errors.push("Password must be at least 12 characters.") }
     if (this.data.password.length > 100) { this.errors.push("Password cannot exceed 100 characters.") }
 
     //Password be at least 8 characters
-    if (this.data.length.username.length > 0 && this.data.password.length < 3) { this.errors.push("Username must be at least 3 characters.") }
+    if (this.data.username.length > 0 && this.data.password.length < 3) { this.errors.push("Username must be at least 3 characters.") }
     if (this.data.username.length > 30) { this.errors.push("Username cannot exceed 30 characters.") }
 }
 User.prototype.register = function () {
