c60842a3b673c1f820cd6407c54e1f6dca3f873b
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-10 07:54:05 -0500

    Install prettier

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,38 +5,38 @@
 // }
 
 // Import User Model
-const User = require('../models/User')
+const User = require("../models/User");
 
 exports.login = (req, res) => {
-    //Create an instance of User object 
-    // And look into the body of the form => req.body for submissions
-    let user = new User(req.body)
-    user.login().then((result) => {
-        req.session.user = { favColor: "red", username: user.data.username }
-        res.send(result)
-    }).catch((error) => {
-        res.send(error)
+  //Create an instance of User object
+  // And look into the body of the form => req.body for submissions
+  let user = new User(req.body);
+  user
+    .login()
+    .then(result => {
+      req.session.user = { favColor: "red", username: user.data.username };
+      res.send(result);
     })
-}
+    .catch(error => {
+      res.send(error);
+    });
+};
 
 exports.register = (req, res) => {
-    let user = new User(req.body)
-    user.register();
+  let user = new User(req.body);
+  user.register();
 
-    if (user.errors.length) {
-        res.send(user.errors)
-    } else {
-        res.send("Congrats there no errors")
-    }
-
-}
-exports.logout = () => {
-
-}
+  if (user.errors.length) {
+    res.send(user.errors);
+  } else {
+    res.send("Congrats there no errors");
+  }
+};
+exports.logout = () => {};
 exports.home = (req, res) => {
-    if (req.session.user) {
-        res.send("Welcome to the actual app!")
-    } else {
-        res.render('home-guest')
-    }
-}
+  if (req.session.user) {
+    res.send("Welcome to the actual app!");
+  } else {
+    res.render("home-guest");
+  }
+};
