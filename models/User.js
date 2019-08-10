a67fca5bd8af731da318a10c87dc5d1a0d3fe175
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-10 07:24:25 -0500

    Password limit and hashed password max of 50 chars

--- models/User.js
+++ models/User.js
@@ -46,11 +46,11 @@ User.prototype.validate = function () {
     if (!validator.isEmail(this.data.email)) { this.errors.push("You must provide a valid email address.") }
     if (this.data.password == "") { this.errors.push("You must provide a password.") }
 
-    //Password be at least 8 characters
-    if (this.data.password.length > 0 && this.data.password.length < 12) { this.errors.push("Password must be at least 12 characters.") }
-    if (this.data.password.length > 100) { this.errors.push("Password cannot exceed 100 characters.") }
+    //Password be at least 5 characters
+    if (this.data.password.length > 0 && this.data.password.length < 5) { this.errors.push("Password must be at least 5 characters.") }
+    if (this.data.password.length > 50) { this.errors.push("Password cannot exceed 50 characters.") }
 
-    //Password be at least 8 characters
+    //Username be at least 3 characters
     if (this.data.username.length > 0 && this.data.password.length < 3) { this.errors.push("Username must be at least 3 characters.") }
     if (this.data.username.length > 30) { this.errors.push("Username cannot exceed 30 characters.") }
 }
