e815a2b9a23f5eea10921c1a82c4d15a0dffce6a
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-09 22:02:11 -0500

    User: Use validator to validate username- only numbers and letters

--- models/User.js
+++ models/User.js
@@ -9,6 +9,7 @@ let User = function (data) {
 
 User.prototype.validate = function () {
     if (this.data.username == "") { this.errors.push("You must provide a username.") }
+    if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) { this.errors.push("Username can only contain letters and numbers") }
     if (!validator.isEmail(this.data.email)) { this.errors.push("You must provide a valid email address.") }
     if (this.data.password == "") { this.errors.push("You must provide a password.") }
 
