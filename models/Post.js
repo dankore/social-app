280b4ee3d518dcaa7b3a96895be11bc1711d6f41
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-11 02:06:35 -0500

    Create post- create prototypes of Post constructor function. cleanUp, validate, create

--- models/Post.js
+++ models/Post.js
@@ -1,8 +1,11 @@
 let Post = (data)=>{
     this.data = data
 }
+Post.prototype.cleanUp = () => {};
+Post.prototype.validate = () => {};
+Post.prototype.create = ()=>{
 
-
+}
 
 
 module.exports = Post
