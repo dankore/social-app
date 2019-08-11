2cae8c54e8d7b9adf4ae501f866ff83d1c09fff5
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-11 16:14:05 -0500

    Profile:find profile by username

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,17 +4,16 @@
 //     logout: () => { }
 // }
 
-exports.mustBeLoggedIn = (req, res, next)=>{
-    if(req.session.user){
-        next()
-
-    }else{
-        req.flash("errors"," You must be logged in to perform that action")
-        req.session.save(()=>{
-        res.redirect('/')
-        })
-    }
-}
+exports.mustBeLoggedIn = (req, res, next) => {
+  if (req.session.user) {
+    next();
+  } else {
+    req.flash("errors", " You must be logged in to perform that action");
+    req.session.save(() => {
+      res.redirect("/");
+    });
+  }
+};
 
 // Import User Model
 const User = require("../models/User");
@@ -26,7 +25,11 @@ exports.login = (req, res) => {
   user
     .login()
     .then(result => {
-      req.session.user = { avatar: user.avatar, username: user.data.username, _id: user.data._id };
+      req.session.user = {
+        avatar: user.avatar,
+        username: user.data.username,
+        _id: user.data._id
+      };
       req.session.save(() => {
         res.redirect("/");
       });
@@ -45,13 +48,17 @@ exports.register = function(req, res) {
   user
     .register()
     .then(() => {
-      req.session.user = { username: user.data.username, avatar: user.avatar, _id: user.data._id};
+      req.session.user = {
+        username: user.data.username,
+        avatar: user.avatar,
+        _id: user.data._id
+      };
       req.session.save(function() {
         res.redirect("/");
       });
     })
     .catch(regErrors => {
-      regErrors.forEach(function (error) {
+      regErrors.forEach(function(error) {
         req.flash("regErrors", error);
       });
       req.session.save(function() {
@@ -77,10 +84,16 @@ exports.home = (req, res) => {
 };
 
 exports.ifUserExists = (req, res, next) => {
-  next();
+  User.findByUserName(req.params.username)
+    .then(userDocument => {
+      req.profileUser = userDocument;
+      next();
+    })
+    .catch(() => {
+      res.render("404");
+    });
 };
 
 exports.profilePostsScreen = (req, res) => {
   res.render("profile");
 };
-
--- models/User.js
+++ models/User.js
@@ -151,4 +151,29 @@ User.prototype.getAvatar = function() {
   this.avatar = `http://gravatar.com/avatar/${md5(this.data.email)}?s=128`;
 };
 
+User.findByUserName = (username)=>{
+  return new Promise((resolve, reject) => {
+    if(typeof(username) != "string"){
+      reject()
+      return
+    }
+    usersCollection.findOne({username: username}).then((userDoc)=>{
+      if(userDoc){
+        userDoc = new User(userDoc, true)
+        userDoc = {
+          _id: userDoc.data._id,
+          username: userDoc.username,
+          avatar: userDoc.avatar
+        }
+        resolve(userDoc)
+      } else {
+        reject()
+      }
+
+    }).catch(()=>{
+      reject()
+    })
+  })
+}
+
 module.exports = User;
