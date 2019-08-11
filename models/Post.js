937307b2b8cfe6f6870a05feb4812c937f16a373
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-11 02:10:55 -0500

    Create post- cleanUp - only strings and define the data we accept- title, body, createddate

--- models/Post.js
+++ models/Post.js
@@ -1,11 +1,22 @@
-let Post = (data)=>{
-    this.data = data
-}
-Post.prototype.cleanUp = () => {};
+let Post = data => {
+  this.data = data;
+};
+Post.prototype.cleanUp = () => {
+  if (typeof this.data.title != "string") {
+    this.data.title = "";
+    if (typeof this.data.body != "string") {
+      this.data.body = "";
+    }
+
+    // Get rid of any bogus property
+    this.data = {
+      title: this.data.title.trim(),
+      body: this.data.body.trim(),
+      createdDate: new Date()
+    };
+  }
+};
 Post.prototype.validate = () => {};
-Post.prototype.create = ()=>{
+Post.prototype.create = () => {};
 
-}
-
-
-module.exports = Post
+module.exports = Post;
