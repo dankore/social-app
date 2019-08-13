8c94ced81576534aa5ae33fabdcc745584745712
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-13 02:27:56 -0500

    Search: Security - install dompurify to sanitize search results since it is delivered through our frontend javascript

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import DOMPurify from "dompurify";
 
 export default class Search {
   // 1. Select DOM elements, and keep track of any useful data
@@ -56,7 +57,7 @@ export default class Search {
   }
   renderResultHTML(posts) {
     if (posts.length) {
-      this.resultsArea.innerHTML = `<div class="list-group shadow-sm">
+      this.resultsArea.innerHTML = DOMPurify.sanitize(`<div class="list-group shadow-sm">
       <div class="list-group-item active"><strong>Search Results</strong> (${
         posts.length > 1 ? `${posts.length} items found` : "1 item found"
       })</div>
@@ -71,11 +72,12 @@ export default class Search {
           }</strong>
         <span class="text-muted small">by ${
           post.author.username
-        } on ${postDate.getMonth() + 1 }/${postDate.getDate()}/${postDate.getFullYear()}</span>
+        } on ${postDate.getMonth() +
+            1}/${postDate.getDate()}/${postDate.getFullYear()}</span>
       </a>`;
         })
         .join("")}
-    </div>`;
+    </div>`);
     } else {
       this.resultsArea.innerHTML = `<p class="alert alert-danger text-center shadow-sm">Sorry, we could not find any results for that search</p>`;
     }
