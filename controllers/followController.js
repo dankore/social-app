e1c6c620111e896ff4266048ac36941b679fc642
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-13 08:38:16 -0500

    Follow: setup a promise and a message for when a follow is successful

--- controllers/followController.js
+++ controllers/followController.js
@@ -1,10 +1,12 @@
-const Follow = require('../models/Follow')
+const Follow = require("../models/Follow");
 
-exports.addFollow = (req, res)=>{
-    let follow = new Follow(req.params.username, req.visitorId)
-    follow.create().then(()=>{
-
-    }).catch(()=>{
-        
+exports.addFollow = (req, res) => {
+  let follow = new Follow(req.params.username, req.visitorId);
+  follow
+    .create()
+    .then(() => {
+      req.flash("success", `Successfully followed ${req.params.username}`);
+      req.session.save(() => res.redirect(`/profile/${req.params.username}`));
     })
-}
+    .catch(() => {});
+};
