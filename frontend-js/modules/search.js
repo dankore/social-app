4b6344c6a3e8ae2b6a04c77d56403451e85beb14
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-12 23:22:57 -0500

    Search prep: add event listener to search icon

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,5 +1,29 @@
 export default class Search {
-    constructor() {
-        alert("Search js is being executed!")
-    }
+  // 1. Select DOM elements, and keep track of any useful data
+  constructor() {
+    this.headerSearchIcon = document.querySelector(".header-search-icon");
+    this.events();
+  }
+
+  // 2. Events
+  events() {
+    this.headerSearchIcon.addEventListener("click", e => {
+      e.preventDefault();
+      this.openOverlay();
+    });
+  }
+
+  // 3. Methods
+  openOverlay() {
+    alert("Overlay method just ran");
+  }
 }
+
+//Above same as below
+
+// function Search(){
+// this,exampleProperty = d
+// }
+
+// Search.prototype.myCoolMethod = function(){}
+// new Search()
