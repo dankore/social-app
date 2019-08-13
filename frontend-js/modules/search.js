d5138e50ee1afccc960375df7de39216623a5e41
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-13 01:47:06 -0500

    Search: Display search fix

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -8,7 +8,7 @@ export default class Search {
     this.overlay = document.querySelector(".search-overlay");
     this.closeIcon = document.querySelector(".close-live-search");
     this.inputField = document.querySelector("#live-search-field");
-    this.resultArea = document.querySelector(".live-search-result");
+    this.resultsArea = document.querySelector(".live-search-results");
     this.loaderIcon = document.querySelector(".circle-loader");
     this.typingWaitTimer;
     this.previousValue = "";
@@ -28,18 +28,20 @@ export default class Search {
   // 3. Methods
   keyPressHandler() {
     let value = this.inputField.value;
-    
+
     if (value == "") {
       clearTimeout(this.typingWaitTimer);
       this.hideLoaderIcon();
       this.hideResultsArea();
     }
+
     if (value != "" && value != this.previousValue) {
       clearTimeout(this.typingWaitTimer);
       this.showLoaderIcon();
       this.hideResultsArea();
-      this.typingWaitTimer = setTimeout(() => this.sendRequest(), 3000);
+      this.typingWaitTimer = setTimeout(() => this.sendRequest(), 750);
     }
+
     this.previousValue = value;
   }
 
@@ -48,60 +50,67 @@ export default class Search {
       .post("/search", { searchTerm: this.inputField.value })
       .then(response => {
         console.log(response.data);
-        this.renderResultHTML(response.data);
+        this.renderResultsHTML(response.data);
       })
       .catch(() => {
-        alert("Hello the request failed.");
+        alert("Hello, the request failed.");
       });
   }
-  renderResultHTML(posts) {
+
+  renderResultsHTML(posts) {
     if (posts.length) {
       this.resultsArea.innerHTML = `<div class="list-group shadow-sm">
-            <div class="list-group-item active"><strong>Search Results</strong> (4 items found)</div>
-
-            <a href="#" class="list-group-item list-group-item-action">
-              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #1</strong>
-              <span class="text-muted small">by barksalot on 0/14/2019</span>
-            </a>
-            <a href="#" class="list-group-item list-group-item-action">
-              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #2</strong>
-              <span class="text-muted small">by brad on 0/12/2019</span>
-            </a>
-            <a href="#" class="list-group-item list-group-item-action">
-              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #3</strong>
-              <span class="text-muted small">by barksalot on 0/14/2019</span>
-            </a>
-            <a href="#" class="list-group-item list-group-item-action">
-              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #4</strong>
-              <span class="text-muted small">by brad on 0/12/2019</span>
-            </a>
-          </div>`;
+      <div class="list-group-item active"><strong>Search Results</strong> (${
+        posts.length > 1 ? `${posts.length} items found` : "1 item found"
+      })</div>
+      ${posts
+        .map(post => {
+          let postDate = new Date(post.createdDate);
+          return `<a href="/post/${
+            post._id
+          }" class="list-group-item list-group-item-action">
+        <img class="avatar-tiny" src="${post.author.avatar}"> <strong>${
+            post.title
+          }</strong>
+        <span class="text-muted small">by ${
+          post.author.username
+        } on ${postDate.getMonth()}/${postDate.getDate()}/${postDate.getFullYear()}</span>
+      </a>`;
+        })
+        .join("")}
+    </div>`;
     } else {
-      this.resultsArea.innerHTML = `<p class="alert alert-danger text-center shadow-sm">Sorry, we could not find any results for that search</p>`;
+      this.resultsArea.innerHTML = `<p class="alert alert-danger text-center shadow-sm">Sorry, we could not find any results for that search.</p>`;
     }
     this.hideLoaderIcon();
     this.showResultsArea();
   }
+
   showLoaderIcon() {
     this.loaderIcon.classList.add("circle-loader--visible");
   }
+
   hideLoaderIcon() {
     this.loaderIcon.classList.remove("circle-loader--visible");
   }
+
   showResultsArea() {
     this.resultsArea.classList.add("live-search-results--visible");
   }
+
   hideResultsArea() {
     this.resultsArea.classList.remove("live-search-results--visible");
   }
+
   openOverlay() {
     this.overlay.classList.add("search-overlay--visible");
-    // settimeout to give other browsers time to respond
     setTimeout(() => this.inputField.focus(), 50);
   }
+
   closeOverlay() {
     this.overlay.classList.remove("search-overlay--visible");
   }
+
   injectHTML() {
     document.body.insertAdjacentHTML(
       "beforeend",
@@ -124,5 +133,3 @@ export default class Search {
     );
   }
 }
-
-
