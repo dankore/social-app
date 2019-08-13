cb19daa55d371e8522757b4abe77f7342921f34f
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-13 08:31:14 -0500

    Follow: followcontroller further setup

--- controllers/followController.js
+++ controllers/followController.js
@@ -1,5 +1,10 @@
 const Follow = require('../models/Follow')
 
 exports.addFollow = (req, res)=>{
+    let follow = new Follow(req.params.username, req.visitorId)
+    follow.create().then(()=>{
 
+    }).catch(()=>{
+        
+    })
 }
