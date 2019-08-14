7af3285a1c0bdc8a4682e0ba8e061f5734ee01cd
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-14 03:15:16 -0500

    Validation registration form: set up files

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -1,7 +1,14 @@
-import Search from './modules/search'
-import Chat from './modules/chat'
+import Search from "./modules/search";
+import Chat from "./modules/chat";
+import RegistrationForm from "./modules/registrationForm";
 
-if(document.querySelector('#chat-wrapper')){
-    new Chat()
+if (document.querySelector("#registration-form")) {
+  new RegistrationForm();
 }
-if(document.querySelector(".header-search-icon")){new Search()}
+
+if (document.querySelector("#chat-wrapper")) {
+  new Chat();
+}
+if (document.querySelector(".header-search-icon")) {
+  new Search();
+}
