4addd72ed632092aed7c152c72d56ac8adb3d4c0
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-14 03:33:05 -0500

    Validation registration form: detect value-changing entry into username box

--- frontend-js/modules/registrationForm.js
+++ frontend-js/modules/registrationForm.js
@@ -1,10 +1,38 @@
 export default class RegistrationForm {
-    constructor(){
-        this.events()
+  constructor() {
+    this.allFields = document.querySelectorAll(
+      "#registration-form .form-control"
+    );
+    this.insertValidationElements();
+    this.username = document.querySelector("#username-register");
+    this.username.previousValue = "";
+    this.events();
+  }
+  // Events
+  events() {
+    this.username.addEventListener("keyup", () => {
+      this.isDifferent(this.username, this.usernameHandler);
+    });
+  }
 
+  // Methods
+  isDifferent(el, handler) {
+    if (el.previousValue != el.value) {
+      handler.call(this);
     }
-    // Events
-    events(){
-        alert("running")
-    }
-}
+    el.previousValue = el.value;
+  }
+
+  usernameHandler() {
+    alert("handle just ran");
+  }
+
+  insertValidationElements() {
+    this.allFields.forEach(el => {
+      el.insertAdjacentHTML(
+        "afterend",
+        '<div class="alert alert-danger small liveValidateMessage"></div>'
+      );
+    });
+  }
+} // End of class function
