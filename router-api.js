9fe2dd1d72d405c9340ff860903a4f0f0f86e596
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-17 00:28:52 -0500

    API: get  post

--- router-api.js
+++ router-api.js
@@ -7,5 +7,6 @@ const followController = require("./controllers/followController");
 apiRouter.post('/login', userController.apiLogin)
 apiRouter.post('/create-post', userController.apiMustBeLoggedIn, postController.apiCreate)
 apiRouter.delete('/post/:id', userController.apiMustBeLoggedIn, postController.apiDelete)
+apiRouter.get('/postsByAuthor/:username', userController.apiGetPostsByUsername)
 
 module.exports = apiRouter
