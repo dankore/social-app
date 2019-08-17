a87bdbee6fee499c262239967c32f896bd4b0f0c
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-17 00:38:34 -0500

    API: open to public to view posts cors is making that possible

--- router-api.js
+++ router-api.js
@@ -2,7 +2,9 @@ const apiRouter = require('express').Router()
 const userController = require("./controllers/userController");
 const postController = require("./controllers/postController");
 const followController = require("./controllers/followController");
+const cors = require('cors')
 
+apiRouter.use(cors())
 
 apiRouter.post('/login', userController.apiLogin)
 apiRouter.post('/create-post', userController.apiMustBeLoggedIn, postController.apiCreate)
