6e1d28ae022b5b2642d47b56e88a9d16feadcf48
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-08-22 15:34:03 -0500

    Connection issue with mongodb fix

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 const MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true })
+MongoClient.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(function(client) {
     module.exports = client;
     const app = require("./app");
