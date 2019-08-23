4ebcb243914b770da6a9a6ad4b5d536e359d3694
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2019-08-22 20:42:22 -0500

    Update postController.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -13,7 +13,7 @@ exports.create = (req, res) => {
     .create()
     .then(newId => {
       sendgrid.send({
-        to: req.session.user.email,
+        to: adamu.dankore@gmail.com,
         from: "adamu.dankore@gmail.com",
         subject: 'Congrats, you just created a new post!',
         text: 'You did a great job of creating a post',
