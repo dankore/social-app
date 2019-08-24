88fe784a697c4ef9968dd5417cf23f2e9a10df45
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2019-08-24 00:15:41 -0500

    Update Comments.js

--- models/Comments.js
+++ models/Comments.js
@@ -2,5 +2,13 @@ const commentsCollection = require("../db")
   .db()
 .collection("comments");
 
+let Comments = function(data){
+  this.data = data;
+}
+
+Comments.prototype.create = function(){
+  return new Promise((resolve, reject)=>{})
+}
+
 
 module.exports = Comments
