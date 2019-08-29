995780a88108684a04ab85693f71800bdf08b247
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2019-08-29 00:25:32 -0500

    Update Thread.js

--- models/Thread.js
+++ models/Thread.js
@@ -105,17 +105,20 @@ Thread.find = function(id) {
     resolve(threads)
   });
 };
-
+// app.post("/delete-item", (req, res) => {
+//   db.collection("items").deleteOne(
+//     { _id: new mongodb.ObjectId(req.body.id) },
+//     () => {
+//       res.send("Success");
+//     }
+//   );
+// });
 
 Thread.delete = (postIdToDelete, currentUserId) => {
   return new Promise(async (resolve, reject) => {
     try {
-      let thread = await Thread.findSingleById(postIdToDelete, currentUserId);
-      if (thread.isVisitorOwner) {
-        await threadCollection.deleteOne({ _id: new ObjectID(postIdToDelete) });
-        resolve();
-      } else {
-        reject();
+        let deleted  = await threadCollection.deleteOne({ _id: new ObjectID(postIdToDelete) });
+        resolve(deleted);
       }
     } catch {
       reject();
@@ -123,23 +126,23 @@ Thread.delete = (postIdToDelete, currentUserId) => {
   });
 };
 
-Thread.findSingleById = function(id, visitorId) {
-  return new Promise(async (resolve, reject) => {
-    if (typeof id != "string" || !ObjectID.isValid(id)) {
-      reject();
-      return;
-    }
-    let thread = await Thread.find(
-      [{ $match: { _id: new ObjectID(id) } }],
-      visitorId
-    );
-    if (thread.length) {
-      resolve(thread[0]);
-    } else {
-      reject();
-    }
-  });
-};
+// Thread.findSingleById = function(id, visitorId) {
+//   return new Promise(async (resolve, reject) => {
+//     if (typeof id != "string" || !ObjectID.isValid(id)) {
+//       reject();
+//       return;
+//     }
+//     let thread = await Thread.find(
+//       [{ $match: { _id: new ObjectID(id) } }],
+//       visitorId
+//     );
+//     if (thread.length) {
+//       resolve(thread[0]);
+//     } else {
+//       reject();
+//     }
+//   });
+// };
 
 
 
