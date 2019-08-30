193b91eddd6723302d883826e544d747b518ac76
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2019-08-30 00:20:45 -0500

    Update Comments.js

--- models/Comments.js
+++ models/Comments.js
@@ -1,1 +1,14 @@
+const commentsCollection = require("../db")
+  .db()
+  .collection("comments");
+const sanitizeHTML = require("sanitize-html");
+const ObjectID = require("mongodb").ObjectID;
+const User = require("./User");
 
+let Comments = function(data){
+  this.data  = data
+  this.errors = []
+}
+
+
+module.exports = Comments
