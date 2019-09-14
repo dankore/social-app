371f0144ae8daf6c132a83f2642413ec5c883108
Adamu M. Dankore <adamu.dankore@gmail.com>
2019-09-14 09:04:30 -0500

    Can close edit form

--- frontend-js/modules/thread-edit.js
+++ frontend-js/modules/thread-edit.js
@@ -1,11 +1,18 @@
 export default class Thread {
   constructor() {
+    this.threadCloseButtons = document.querySelectorAll(".thread-close-button")
     this.editButtons = document.querySelectorAll(".edit-button");
     this.editForms = document.querySelectorAll(".edit-form");
     Array.from(this.editButtons).map((item, idx)=>{
         return this.editButtons[idx].addEventListener('click', () => {
-          Array.from(this.editForms).forEach(item2 =>{
+          Array.from(this.editForms).forEach(item2 => {
             return this.editForms[idx].classList.add("edit-form--visible");
+          })
+          Array.from(this.threadCloseButtons).map(item3 => {
+            return this.threadCloseButtons[idx].addEventListener('click', (e) => {
+              console.log(e.target)
+              return this.editForms[idx].classList.remove("edit-form--visible");
+            })
           })
         })
     })
